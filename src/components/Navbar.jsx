0b207f5ccdf310490aa6f83cd2d50bea281badf4
Chandrashekharsk <cs9981625252@gmail.com>
2024-08-26 14:12:32 +0530

    change authentication button color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -133,11 +133,11 @@ const Navbar = () => {
                         </button>
                         {
                             !user ? (
-                                <Link to={"/sign-in"} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+                                <Link to={"/sign-in"} className="bg-green-600 text-sm text-white px-4 py-2 rounded-md hover:bg-green-500 focus:outline-none">
                                     Sign In
                                 </Link>
                             ) : (
-                                <button onClick={handleLogoutClick} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+                                <button onClick={handleLogoutClick} className="bg-green-600 text-sm text-white px-4 py-2 rounded-md hover:bg-green-500 focus:outline-none">
                                     Sign Out
                                 </button>
                             )
